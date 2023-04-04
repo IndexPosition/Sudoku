671721a3882b2e1c5c7b0e8c1a7f83375010cfaa
Abhijith Prakash <99633082+IndexPosition@users.noreply.github.com>
2023-04-04 12:34:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
+const BOARD_SIZE = 9;
+
 const sudokuBoard = document.getElementById("sudoku-board");
 
-const cells = new Array(9);
-for (let i = 0; i < cells.length; i++) {
-  cells[i] = new Array(9);
-}
-for (let m = 0; m < 3; m++) {
-    const colgroup = document.createElement("colgroup");
-    for (let n = 0; n < 3; n++) {
-      colgroup.appendChild(document.createElement("col"));
-    }
-    sudokuBoard.appendChild(colgroup);
-  }
-  
+const cells = [];
 for (let k = 0; k < 3; k++) {
   const tbody = document.createElement("tbody");
+  sudokuBoard.appendChild(tbody);
   for (let i = 0; i < 3; i++) {
     const row = document.createElement("tr");
-    for (let j = 0; j < 9; j++) {
+    cells.push([]);
+    for (let j = 0; j < BOARD_SIZE; j++) {
       const cell = document.createElement("td");
       cell.contentEditable = true;
-      cell.addEventListener("input", () => {
-        const validInput = /[1-9]/.test(cell.textContent)? cell.textContent[0]: "";
-        cell.textContent = validInput;
-      });
-      cells[k*3+i][j] = cell; 
+      cells[cells.length - 1].push(cell);
       row.appendChild(cell);
     }
-    tbody.appendChild(row); 
+    tbody.appendChild(row);
   }
-  sudokuBoard.appendChild(tbody);   
+}
+
+sudokuBoard.addEventListener("input", (event) => {
+  const cell = event.target;
+  const validInput = /[1-9]/.test(cell.textContent) ? cell.textContent[0] : "";
+  cell.textContent = validInput;
+});
+
+for (let m = 0; m < 3; m++) {
+  const colgroup = document.createElement("colgroup");
+  colgroup.innerHTML = '<col>'.repeat(3);
+  sudokuBoard.appendChild(colgroup);
 }
